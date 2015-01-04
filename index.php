340282946e68c7f2c3231ab4adc090d571f63abf
paulq <paulq@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-04 18:29:10 +0000

    Adding new files
    git-svn-id: https://plugins.svn.wordpress.org/chatwee/trunk@1059675 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,141 @@
+<?php
+
+	ob_start();
+
+	/*
+	Plugin Name: Chatwee
+	Description: Chatwee is fully customizable social chat & comment platform for website and blogs. With Chatwee you can engage your online community and provide real-time communication.
+	Author: pawelq
+	Version: 1.0.2
+	Author URI: http://chatwee.com
+	*/
+
+
+	register_activation_hook( __FILE__, 'set_up_options' );
+
+	function set_up_options(){
+		register_setting( 'chatwee-settings-group', 'is_home' ); 
+		register_setting( 'chatwee-settings-group', 'is_search' );
+		register_setting( 'chatwee-settings-group', 'is_archive' );
+		register_setting( 'chatwee-settings-group', 'is_page' );
+		register_setting( 'chatwee-settings-group', 'is_single' );
+		register_setting( 'chatwee-settings-group', 'ssostatus' );
+		register_setting( 'chatwee-settings-group', 'clientid' );
+		register_setting( 'chatwee-settings-group', 'keyapi' );
+		
+		update_option('chatwee-settings-group[is_home]',"on");
+		update_option('chatwee-settings-group[is_search]',"on");
+		update_option('chatwee-settings-group[is_archive]',"on");
+		update_option('chatwee-settings-group[is_single]',"on");
+		update_option('chatwee-settings-group[is_page]',"on");
+		update_option('chatwee-settings-group[ssostatus]',"on");
+		update_option('chatwee',"");
+		
+		}
+
+		add_action('admin_menu', 'chatwee_create_menu');   
+	
+	if (check_sso_enable())
+		add_action('wp_login', 'remote_login',7,2);
+	if (check_sso_enable())
+		add_action('wp_logout', 'remote_logout',7);
+	
+
+	function chatwee_create_menu() 
+	{
+		add_menu_page('Account Configuration', 'Chatwee', 'administrator', 'chatwee_panel', 'chatwee_panel',  plugins_url( '/ico_fb_small_16x16.png', __FILE__ ));
+	}
+	
+	function check_sso_enable()
+	{
+		$option = get_option('chatwee-settings-group[ssostatus]');
+		$status = ($option == 'on') ? true : false;
+		return $status;
+	}
+	
+	function get_the_user_ip()
+	{
+	
+		if ( ! empty( $_SERVER['HTTP_CLIENT_IP'] ) ) {
+			//check ip from share internet
+			$ip = $_SERVER['HTTP_CLIENT_IP'];
+		} elseif ( ! empty( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
+		//to check ip is pass from proxy
+			$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+		} else {
+			$ip = $_SERVER['REMOTE_ADDR'];
+		}
+		
+		return $ip;
+	}
+	
+	function get_avatar_url($user_id)
+	{
+
+		$img = get_avatar($user_id);
+		preg_match("/src='(.*?)'/i", $img, $matches);
+		$url = $matches[1];
+		
+		return $url;
+	}
+
+	function remote_logout() 
+	{
+
+		$chatId = get_option('chatwee-settings-group[clientid]');
+		$clientKey = get_option('chatwee-settings-group[keyapi]');
+		$sessionId = $_COOKIE['chch-SI'];
+		$url = "http://chatwee-api.com/api/remotelogout?chatId=".$chatId."&clientKey=".$clientKey."&sessionId=".$sessionId;
+		file_get_contents($url);
+		setcookie("chch-SI", "", time() - 1);
+	}
+
+	function remote_login($user_login, $user)
+	{
+		
+		$chatId = get_option('chatwee-settings-group[clientid]');
+		$clientKey = get_option('chatwee-settings-group[keyapi]');
+		$isAdmin = (is_super_admin( $user->ID) ? 1 : 0);
+		$ip = get_the_user_ip();
+		$avatar = get_avatar_url($user->ID);
+		$url = "http://chatwee-api.com/api/remotelogin?chatId=".$chatId."&clientKey=".$clientKey."&login=".$user_login."&isAdmin=".$isAdmin."&ipAddress=".$ip."&avatar=".$avatar;
+		$response = file_get_contents($url);
+		$sessionArray = json_decode($response);
+		$sessionId = $sessionArray->sessionId;
+		setcookie("chch-SI", $sessionId, time() + 2592000, "/");
+	}
+
+
+	function chatwee_panel() 
+	{ 
+		include ('chatwee.php');
+	}
+		
+	function chatwee_embedchat()
+	{
+		echo get_option('chatwee');	
+	}
+
+	function check_site(){
+	
+	if(get_option( 'chatwee-settings-group[is_home]') == "on" && is_home()) {
+        add_action( 'wp_footer', 'chatwee_embedchat' );
+    }
+	 if(get_option( 'chatwee-settings-group[is_page]') == "on" && is_page()) {
+        add_action( 'wp_footer', 'chatwee_embedchat' );
+    }
+	 if(get_option( 'chatwee-settings-group[is_search]') == "on" && is_search()) {
+        add_action( 'wp_footer', 'chatwee_embedchat' );
+    }
+	 if(get_option( 'chatwee-settings-group[is_single]') == "on" && is_single()) {
+        add_action( 'wp_footer', 'chatwee_embedchat' );
+    }
+	 if(get_option( 'chatwee-settings-group[is_archive]') == "on" && is_archive()) {
+        add_action( 'wp_footer', 'chatwee_embedchat' );
+    }
+	}
+	
+	add_action("wp_footer","check_site", 5);
+
+
+?>
